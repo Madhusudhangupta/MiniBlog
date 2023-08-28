6a0c26b2ca72438d9d82a4ed327f7560463e91e2
Madhusudhan <madhusudhangupta18@gmail.com>
2023-08-28 22:39:31 +0530

    Added files

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0*j266=h4wkezyi_0)n74slr)-u*&ah!^c6vv@z)vf&-24jxda
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
